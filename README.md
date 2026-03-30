0d090a2084f376df9895ccc028f6b7e15ec2450d
NIKHIL KUMAR <nikhil.fut.dev@gmail.com>
2026-03-30 23:49:14 +0530

    Sweet Boxes

---
+++ README.md
@@ -0,0 +1,1 @@
+"This Project is a clone of Amazon for educational purposes only. Not affiliated with Amazon."
---
+++ footer.js
@@ -0,0 +1,9 @@
+const footerTitles = document.querySelectorAll(".footer-col h4");
+
+footerTitles.forEach((title) => {
+  title.addEventListener("click", () => {
+    const parent = title.parentElement;
+
+    parent.classList.toggle("active");
+  });
+});
